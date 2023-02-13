53e6f20583bbb1f90caedd0701be7b9bbce9a641
fiona-tyy <zyyfiona@hotmail.com>
2023-02-13 10:59:03 +0800

    app without server

--- src/main/java/revision/App.java
+++ src/main/java/revision/App.java
@@ -1,15 +1,37 @@
 package revision;
 
+import java.util.Random;
+import java.util.Scanner;
 
-public final class App {
+public final class App implements Runnable{
     private App() {
     }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        // Need the random class to carry out randomised operation
+        Random random = new Random();
+
+        // generate random number between 0 and 99 (specified number is exclusive)
+        int randomNumber = random.nextInt(100);
+
+        // create variable to store guess
+        int myGuess = 0;
+
+        // Expect input from keyboard
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Guess a number between 0 and 99");
+        while(myGuess != randomNumber){
+            myGuess = scanner.nextInt();
+            if (myGuess < randomNumber){
+                System.out.println("Your guess is too low");
+            } else if (myGuess > randomNumber){
+                System.out.println("Your guess is too high");
+            } else {
+                System.out.println("You have guessed it right!");
+                scanner.close();
+            }
+        }
+
     }
 }
